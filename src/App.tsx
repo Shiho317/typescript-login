b3a84bf5b0375411635f01670d7383ab4afa7cff
Shiho Nagano <shiho@nagano.com>
2022-01-23 20:28:31 -0800

    added props

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,40 @@ import {
 } from "react-router-dom";
 import SignUp from './SignUp/SignUp';
 import Home from './Home/Home';
+import { useState } from 'react';
+import datas from './data.json';
 
 export type UserInfo = {
-  name: string;
-  email: string;
-  password: string;
-  consfirm: string;
+    name: string;
+    email: string;
+    password: string;
+    consfirm: string;
+    completed: boolean;
 }
 
 function App() {
+
+  const [isEmail, setIsEmail] = useState('')
+  const [isPassword, setIsPassword] = useState('');
+
+  const Completed = (user: UserInfo) => {
+    const findUser = datas.filter(data => data.email === user.email)
+    console.log(findUser)
+  }
+
+  const [isLogin, setIsLogIn] = useState(false);
+
+
+  const IsLogIn = () => {
+
+  }
+
   return (
     <Wrapper>
       <Router>
         <Routes>
-          <Route path="/" element={<SignUp/>}/>
-          <Route path="/login" element={<SignIn/>}/>
+          <Route path="/" element={<SignUp isEmail={isEmail} Completed={Completed}/>}/>
+          <Route path="/login" element={<SignIn />}/>
           <Route path="/home" element={<Home/>}/>
         </Routes>
       </Router>
--- src/SignIn/SignIn.tsx
+++ src/SignIn/SignIn.tsx
@@ -4,20 +4,21 @@ import Button from '@mui/material/Button';
 import { UserInfo } from '../App';
 
 type Props = {
-  user: UserInfo;
+  isEmail: UserInfo;
+  Completed: (user: UserInfo) => void;
 }
 
-const SignIn: React.FC<Props> = ({user}) => {
+const SignIn: React.FC<Props> = ({isEmail, Completed}) => {
 
   return(
     <Wrapper>
       <h3>Log In</h3>
       <div className='contents-wrap'>
         <div className='email-input'>
-          <TextField fullWidth id="outlined-basic" label="email" variant="outlined" />
+          <TextField fullWidth id="outlined-basic" label="email" variant="outlined" value={isEmail} />
         </div>
         <div className='password-input'>
-          <TextField fullWidth id="outlined-basic" label="password" variant="outlined" />
+          <TextField fullWidth id="outlined-basic" label="password" variant="outlined" value={isPassword} />
         </div>
         <div className='submit-btn'>
           <Button fullWidth variant="contained" disableElevation>
